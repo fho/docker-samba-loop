f9ee60ba4ec961838ca587416f4a9f81438671aa
Fabian Holler <fabian.holler@simplesurance.de>
2017-09-01 11:21:20 +0200

    fixup! run more clients, do I/O from clients

--- run-client.sh
+++ run-client.sh
@@ -2,6 +2,7 @@
 
 docker build -t client-smb:1 .
 
-for ((i=0; i <10; i++)); do
+while true; do
 	( for ((j=0; j <10; j++)); do docker run --rm --name client-smb-$i-$j --privileged --link samba:samba client-smb:1; done ) &
+	wait
 done
